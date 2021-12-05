8f51cafa610fa46c700f256645a7c1aebe214b9a
Koraki_Mx <64110443+YoshiZH@users.noreply.github.com>
2021-12-04 17:00:32 -0700

    UPDATE

--- Drivers/BSP.h
+++ Drivers/BSP.h
@@ -1,18 +1,10 @@
- // FileName:        BSP.h
- // Dependencies:    None.
- // Processor:       MSP432
- // Board:			 MSP432P401R
- // Program version: CCS V8.3 TI
- // Company:         Texas Instruments
- // Description:     Incluye la capa de abstracción de los drivers.
- // Authors:         José Luis Chacón M. y Jesús Alejandro Navarro Acosta.
- // Updated:         11/2018
+
 
 #ifndef BSP_H_
 #define BSP_H_
 
 //#include "msp.h"
-// Si hay problemas con los 'includes', busque la siguiente dirección.
+// Si hay problemas con los 'includes', busque la siguiente direcciÃ³n.
 #include <ti/devices/msp432p4xx/inc/msp.h>
 
 #include "../Drivers/UART.h"
