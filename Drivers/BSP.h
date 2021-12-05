e5ba4591942c2024909cd5e7872a7d0f2f0bb351
Koraki_Mx <64110443+YoshiZH@users.noreply.github.com>
2021-12-04 17:35:27 -0700

    UPDATED

--- Drivers/BSP.h
+++ Drivers/BSP.h
@@ -3,9 +3,9 @@
 #ifndef BSP_H_
 #define BSP_H_
 
-//#include "msp.h"
+#include "msp.h"
 // Si hay problemas con los 'includes', busque la siguiente dirección.
-#include <ti/devices/msp432p4xx/inc/msp.h>
+//#include <ti/devices/msp432p4xx/inc/msp.h>
 
 #include "../Drivers/UART.h"
 
